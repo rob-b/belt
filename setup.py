15ff701cbb931a5ed045a1db26f910f24f44cac7
Rob Berry <rob@luvhate.us>
2013-03-24 12:17:23 +0000

    Use project setup from a better pcreate template

--- setup.py
+++ setup.py
@@ -6,7 +6,16 @@ here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
-requires = ['pyramid>=1.0.2', 'pyramid_jinja2']
+requires = [
+    'pyramid',
+    'pyramid_jinja2',
+    'SQLAlchemy',
+    'transaction',
+    'pyramid_tm',
+    'pyramid_debugtoolbar',
+    'zope.sqlalchemy',
+    'waitress',
+]
 
 setup(name='belt',
       version='0.0',
@@ -14,23 +23,23 @@ setup(name='belt',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
           "Programming Language :: Python",
-          "Framework :: Pylons",
+          "Framework :: Pyramid",
           "Topic :: Internet :: WWW/HTTP",
           "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
       ],
       author='',
       author_email='',
       url='',
-      keywords='web pyramid pylons',
+      keywords='web wsgi bfg pylons pyramid',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
+      test_suite='belt',
       install_requires=requires,
-      tests_require=requires,
-      test_suite="belt",
       entry_points="""\
       [paste.app_factory]
       main = belt:main
+      [console_scripts]
+      initialize_belt_db = belt.scripts.initializedb:main
       """,
-      paster_plugins=['pyramid'],
       )
