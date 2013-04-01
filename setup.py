7337e7934e334157d212b0524cde28c3b6770a3a
Rob Berry <rob@luvhate.us>
2013-04-01 17:11:57 +0000

    Back to development: 0.3

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ test_requires = [
 ]
 
 setup(name='belt',
-      version='0.2',
+      version='0.3.dev0',
       description='belt',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
