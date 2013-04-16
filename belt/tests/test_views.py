3ae6dfaa380f9c7bbf6ab99979104eba065d96a4
Rob Berry <rob@luvhate.us>
2013-04-16 22:26:16 +0000

    Move fixtures to conftest.py

--- belt/tests/test_views.py
+++ belt/tests/test_views.py
@@ -1,5 +1,3 @@
-import os
-import pytest
 import py.test
 from pyramid import testing
 from pyramid.httpexceptions import HTTPNotFound, HTTPServerError
@@ -7,11 +5,6 @@ from httpretty import HTTPretty
 from belt.utils import pypi_url
 from belt import models
 from belt.axle import split_package_name
-from pyramid.paster import get_appsettings
-from sqlalchemy import engine_from_config
-
-
-ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
 
 
 pypi_base_url = 'https://pypi.python.org/packages'
@@ -33,52 +26,6 @@ def create_package(path, content=u''):
     package.releases.append(rel)
     rel.files.append(models.File(filename=filename, md5='gg'))
     return package
-
-
-@pytest.fixture
-def http(request):
-    HTTPretty.reset()
-    HTTPretty.enable()
-    request.addfinalizer(HTTPretty.disable)
-
-
-@pytest.fixture(scope='session')
-def connection():
-    settings = get_appsettings(os.path.join(ROOT, 'test.ini'))
-    engine = engine_from_config(settings, 'sqlalchemy.')
-    engine.echo = False
-    models.Base.metadata.create_all(engine)
-    connection = engine.connect()
-    models.DBSession.registry.clear()
-    models.DBSession.configure(bind=connection)
-    models.Base.metadata.bind = engine
-    return connection
-
-
-@pytest.fixture
-def db_session(request, connection):
-    from transaction import abort
-    trans = connection.begin()
-    request.addfinalizer(trans.rollback)
-    request.addfinalizer(abort)
-
-    from belt.models import DBSession
-    return DBSession
-
-
-@pytest.fixture
-def config(request):
-    from pyramid import testing  # noqa
-    config = testing.setUp()
-    request.addfinalizer(testing.tearDown)
-    return config
-
-
-@pytest.fixture
-def dummy_request(config, tmpdir):
-    config.add_settings({'local_packages': str(tmpdir)})
-    config.manager.get()['request'] = request = testing.DummyRequest()
-    return request
 
 
 class TestView(object):
