c35387314ef44fd11c4cdeb62db68ee9a893b402
Rob Berry <rob@luvhate.us>
2013-10-24 20:47:40 +0000

    Back to development: 0.7

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ test_requires = [
 ]
 
 setup(name='belt',
-      version='0.6',
+      version='0.7.dev0',
       description='belt',
       license='BSD',
       long_description=README + '\n\n' + CHANGES,
